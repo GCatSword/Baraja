ced56885d6bf19c0188868c06d76674213fdb379
GCatSword <61329887+GCatSword@users.noreply.github.com>
2020-04-27 22:14:35 +0200

    Modulo cartas

--- main.py
+++ main.py
@@ -1,24 +1,9 @@
-palos = ('o','c','e','b')
-cartas = ('A','2','3','4','5','6','7','S','C','R')
-
-pf = ('D','C','P','T')
-cf = ('A','2','3','4','5','6','7','8','9','10','J','Q','K')
-
-
-def crea_baraja():
-    baraja = []
-
-    for palo in palos:
-        for carta in cartas:
-            naipe = carta + palo
-            baraja.append(naipe)
-
-    return baraja
-
-
+from cartas import crea_baraja
+#import cartas
 
 baraja1 = crea_baraja()
 baraja2 = crea_baraja()
 
 print(baraja1)
 print(baraja2)
+
